755a0e928d1536953c343416389f139e3c68e681
dario <darionovantadue@gmail.com>
2017-11-02 15:49:23 +0100

    Rendering page

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from utils import fibonacci
 from data import models, db
 
@@ -7,37 +7,42 @@ app = Flask(__name__)
 db_source = db.db_source
 
 
-@app.route('/hello_world', methods=['GET'])
+@app.route('/api/hello_world', methods=['GET'])
 def hello():
     return jsonify({'hello': 'World'})
 
 
-@app.route('/hello/<user>', methods=['GET'])
+@app.route('/api/hello/<user>', methods=['GET'])
 def hello_user(user: str):
     return jsonify({'hello': '{}'.format(user)})
 
 
-@app.route('/fib30', methods=['GET'])
+@app.route('/api/fib30', methods=['GET'])
 def fib30():
     return jsonify({'fibonacci30': fibonacci.fibonacci(30)})
 
 
-@app.route('/single_user', methods=['GET'])
+@app.route('/api/single_user', methods=['GET'])
 def single_user():
     user = db.session.query(models.Person).first()
     return jsonify({'user': models.__get_user(user)})
 
 
-@app.route('/all_users', methods=['GET'])
+@app.route('/api/all_users', methods=['GET'])
 def all_users():
     users = [models.__get_user(u) for u in db.session.query(models.Person).all()]
     return jsonify({'users': users})
 
 
-@app.route('/users_with_cities', methods=['GET'])
+@app.route('/api/users_with_cities', methods=['GET'])
 def usrs_w_cities():
     u_w_c = db.session.query(models.Person, models.City).filter(models.Person.city_id == models.City.id).all()
     return jsonify({'users_with_cities': [models.__get_user_w_city(u) for u in u_w_c]})
+
+
+@app.route('/hello_page')
+def hello_page():
+    return render_template('hello_page.html')
 
 
 if __name__ == '__main__':
